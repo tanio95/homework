04562961fca0311476621e6c2927171a3bf7b4f4
tanio95 <tanio995@gmail.com>
2016-07-18 16:09:39 +0300

    jquery

---
+++ jquery homework/script.js
@@ -0,0 +1,34 @@
+/* 1va zadacha
+	
+	$(document).ready(function(){
+    $('.btn1').click(function(){
+    $('p').show();
+    });
+}); 
+
+*/
+
+/* 3-ta zadacha $(document).ready(function(){
+    $('.btn2').click(function(){
+        $('.yellow').fadeIn(200);
+    });
+}); */
+
+ /* 4-ta zadacha $(document).ready(function(){
+    $('.btn3').click(function(){
+        $('.yellow').fadeOut(200);
+    });
+}); */
+
+/* 5-ta zadacha $(document).ready(function(){
+    $(".btn4").hover(function(){
+    $(".red").slideToggle("slow");
+    });
+}); */
+
+/* 6-ta zadacha $(document).ready(function(){
+    $("button").click(function(){
+    $("h1,h2").addclass("blue");
+    });
+});
+*/
