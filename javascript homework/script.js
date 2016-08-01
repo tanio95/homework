6847438c3e5cdc9bd6e9d2cb8a9504eeaf96095b
tanio95 <tanio995@gmail.com>
2016-08-01 21:07:10 +0300

    oshte 3 zadachi javascript

--- javascript homework/script.js
+++ javascript homework/script.js
@@ -37,3 +37,51 @@ var student = {
 }
 console.log(student);
 */
+
+
+/* 3-ta zadcha
+var a = 4;
+var b = 7;
+if(7%2 == 0)
+{
+	console.log("b = deli se");
+}
+else
+{
+	console.log("b = ne se deli");
+} 
+if(4%2 == 0)
+{
+	console.log("a = deli se");
+}
+else
+{
+	console.log("a = ne se deli");
+}
+*/
+/* 7ma zadacha
+for ( var i = 50; i<=100; i++)
+{
+	console.log(i);
+
+	 if(i%7 == 0)
+	{
+		console.log("se deli na 7");
+	}
+}
+*/
+/* 8ma zadacha
+for ( var i = 20; i<=60; i++)
+{
+	console.log(i);
+
+	 if(i%4 == 0)
+	{
+		console.log("se deli na 4");
+	}
+	if (i%9 == 0) 
+	{
+		console.log("se deli na 9");
+	}
+
+}
